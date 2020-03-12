36cbde3379c315b749920a539d9589fb6f5a014a
fred <fredfrede414@gmail.com>
2020-03-13 02:55:13 +0400

    demoappforall

--- lib/main.dart
+++ lib/main.dart
@@ -1,74 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-
-import './screens/splash_screen.dart';
-import './screens/cart_screen.dart';
-import './screens/products_overview_screen.dart';
-import './screens/product_detail_screen.dart';
-import './providers/products.dart';
-import './providers/cart.dart';
-import './providers/orders.dart';
-import './providers/auth.dart';
-import './screens/orders_screen.dart';
-import './screens/user_products_screen.dart';
-import './screens/edit_product_screen.dart';
-import './screens/auth_screen.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider.value(
-          value: Auth(),
-        ),
-        ChangeNotifierProxyProvider<Auth, Products>(
-          update: (ctx, auth, previousProducts) => Products(
-            auth.token,
-            auth.userID,
-            previousProducts == null ? [] : previousProducts.items,
-          ),
-        ),
-        ChangeNotifierProvider.value(
-          value: Cart(),
-        ),
-        ChangeNotifierProxyProvider<Auth, Orders>(
-          update: (ctx, auth, previousOrders) => Orders(
-            auth.token,
-            auth.userID,
-            previousOrders == null ? [] : previousOrders.orders,
-          ),
-        ),
-      ],
-      child: Consumer<Auth>(
-        builder: (ctx, authData, _) => MaterialApp(
+    return MaterialApp(
           debugShowCheckedModeBanner: false,
-          title: 'MyShop',
+          title: 'Demo',
           theme: ThemeData(
             primarySwatch: Colors.purple,
             accentColor: Colors.deepOrange,
-            fontFamily: 'Lato',
           ),
-          home: authData.isAuth
-              ? ProductsOverviewScreen()
-              : FutureBuilder(
-                  future: authData.tryAutoLogin(),
-                  builder: (ctx, authSnapShot) =>
-                      authSnapShot.connectionState == ConnectionState.waiting
-                          ? SplashScreen()
-                          : AuthScreen(),
-                ),
+          home: Scaffold(),
           routes: {
-            ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
-            CartScreen.routeName: (ctx) => CartScreen(),
-            OrdersScreen.routeName: (ctx) => OrdersScreen(),
-            UserProductsScreen.routeName: (ctx) => UserProductsScreen(),
-            EditProductScreen.routeName: (ctx) => EditProductScreen(),
           },
-        ),
-      ),
     );
   }
 }
--- lib/urls.dart
+++
@@ -1,7 +0,0 @@
-const baseUrl="https://flutter-travelapp.firebaseio.com/";
-const productsUrl=baseUrl+"products.json";
-const ordersUrl=baseUrl+"orders/";
-const apiKey="AIzaSyB-UCjmigpNaRWhYnKe0RmoqE0e1o0YVhM";
-const authUrl="https://identitytoolkit.googleapis.com/v1/accounts:";
-const signupUrl="signUp?key=$apiKey";
-const signIn="signInWithPassword?key=$apiKey";
